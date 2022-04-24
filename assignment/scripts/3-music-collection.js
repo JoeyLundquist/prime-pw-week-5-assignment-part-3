dabfb1be059e06a9ec0168713c1d8168a560fa26
Joey Lundquist <joeylundquist91@gmail.com>
2022-04-24 13:04:18 -0500

    made the collection array as well as made the addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,15 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished){
+    let album = {
+        Title: title,
+        Artist: artist, 
+        YearPublished: yearPublished
+    }
+    collection.push(album);
+}
+
+addToCollection('The Prophecy', 'NSP', 2020);
+console.log(collection)
