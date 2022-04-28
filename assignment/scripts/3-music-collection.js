027b4e3b4a3f0ecef68d333f081a4e3e05502619
Joey Lundquist <joeylundquist91@gmail.com>
2022-04-27 22:38:47 -0500

    I think i have the base funcitonality for the first stretch goal complete

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -43,16 +43,22 @@ console.log(findByArtist('NSP'));
 console.log(findByArtist('Eminem'));
 console.log(findByArtist('Wookiefoot'));
 
-function search(question){
+function search(query){
     let results = [];
     for(let i=0; i<=collection.length-1; i++){
-        if(question === collection[i].Title || question === collection[i].Artist || question === collection[i].YearPublished){
+        if(query == null || query == ""){
+            results = collection
+        }
+        else if(query === collection[i].Title || query === collection[i].Artist || query === collection[i].YearPublished){
             results.push(collection[i])
         }
     }
     return results;
 }
 
+console.log('Testing my search function')
 console.log(search(2020));
 console.log(search(1991));
 console.log(search('The Prophecy'));
+console.log(search('NSP'));
+console.log(search());
